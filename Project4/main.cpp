018e926244e500b66c0857dbf41578b033770b7c
Enoch2090 <gyc990926@gmail.com>
2020-12-17 17:45:38 +0800

    P4

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -19,6 +19,7 @@ struct Node {
     uInt index = 0;
     uInt dst = UINT_MAX / 2;
     uInt weight = UINT_MAX;
+    uInt indegree = 0;
     bool visited = false;
     Node* next = nullptr;
     Node* predecessor = nullptr; // only used for searching shortest path
@@ -30,11 +31,14 @@ struct Node {
         currNode->next = new Node();
         currNode->next->index = destination->index;
         currNode->next->weight = w;
-    }
+    };
     ~Node() {
         if (!this->next) return;
         delete this->next;
-    }
+    };
+    struct compareNode{
+        bool operator() (Node *n1, Node *n2) { return n1->index>n2->index; }
+    };
 };
 
 typedef pair<uInt, Node*> DNPair;
@@ -49,50 +53,136 @@ private:
     uInt start;
     uInt destination;
     vector<Node*> adjList;
+    vector<Node*> udAdjList;
 public:
     explicit Graph(uInt size, uInt start, uInt destination, vector<Edge> v) {
         this->size = size;
         this->start = start;
         this->destination = destination;
-        for (uInt i = 0; i<size; i++) {
+        for (uInt i = 0; i<size; i++) { // Create Nodes
             Node* currNode = new Node();
             currNode->index = i;
             this->adjList.push_back(currNode);
+            currNode = new Node();
+            currNode->index = i;
+            this->udAdjList.push_back(currNode);
         }
-        for (auto& edge : v) {
+        for (auto& edge : v) { // Add connections
             adjList[edge.start]->addEdge(adjList[edge.destination], edge.weight);
+            adjList[edge.destination]->indegree++;
+            udAdjList[edge.start]->addEdge(adjList[edge.destination], edge.weight);
+            udAdjList[edge.destination]->addEdge(adjList[edge.start], edge.weight);
         }
-        cout << "INIT DONE" << endl;
     };
 
     ~Graph() {
         for (uInt i = 0; i<size; i++) {
             delete this->adjList[i];
+            delete this->udAdjList[i];
+        }
+    };
+
+    void resetStatus() {
+        for (uInt i=0; i<this->size; i++) {
+            this->adjList[i]->dst = UINT_MAX/2;
+            this->adjList[i]->visited = false;
+            this->udAdjList[i]->dst = UINT_MAX/2;
+            this->udAdjList[i]->visited = false;
         }
     }
 
-    void findShortestPath() {
+    void findShortesPath() {
+        resetStatus();
+        this->adjList[start]->dst = 0;
         priority_queue<DNPair, vector<DNPair>, comparePair> q;
         q.push(make_pair(0, this->adjList[start]));
         while (!q.empty()) {
             DNPair top = q.top();
             q.pop();
-            cout << "POP" << endl;
-            if (top.second->index != top.first) {
+            Node* v = top.second;
+            if (v->dst != top.first) {
                 continue;
             } // handle duplicates
-            Node* currNode = top.second->next;
-            while (currNode) {
-                if (!adjList[currNode->index]->visited && adjList[currNode->index]->dst > top.second->dst + currNode->weight) {
-                    adjList[currNode->index]->dst = top.second->dst + currNode->weight;
-                    q.push(make_pair(adjList[currNode->index]->dst, adjList[currNode->index]));
+            Node* u = v->next;
+            while (u!= nullptr) {
+                if (!adjList[u->index]->visited && adjList[u->index]->dst > v->dst + u->weight) {
+                    adjList[u->index]->dst = v->dst + u->weight;
+                    q.push(make_pair(adjList[u->index]->dst, adjList[u->index]));
                 }
-                currNode = currNode->next;
+                u = u->next;
             }
             top.second->visited = true;
         }
-        cout << adjList[this->destination]->dst;
+        if (adjList[this->destination]->dst==UINT_MAX/2) {
+            cout << "No path exists!\n";
+            return;
+        }
+        cout << "Shortest path length is " << adjList[this->destination]->dst << "\n";
     };
+
+    void isDAG() {
+        queue<Node*> q, o;
+        for (uInt i=0; i<this->size; i++) {
+            if (adjList[i]->indegree==0) {
+                q.push(adjList[i]);
+            }
+        }
+        while (!q.empty()) {
+            Node* currNode = q.front();
+            q.pop();
+            o.push(currNode);
+            Node* neighborNode = currNode->next;
+            while(neighborNode) {
+                adjList[neighborNode->index]->indegree--;
+                neighborNode = neighborNode->next;
+            }
+            neighborNode = currNode->next;
+            while(neighborNode) {
+                if (adjList[neighborNode->index]->indegree==0) { q.push(adjList[neighborNode->index]); }
+                neighborNode = neighborNode->next;
+            }
+        }
+        if (o.size()!=this->size){
+            cout << "The graph is not a DAG\n";
+            return;
+        }
+        cout << "The graph is a DAG\n";
+    };
+
+    void findMST() {
+        resetStatus();
+        uInt weightSum = 0;
+        this->udAdjList[start]->dst = 0;
+        priority_queue<DNPair, vector<DNPair>, comparePair> q;
+        q.push(make_pair(0, this->udAdjList[start]));
+        while (!q.empty()) {
+            DNPair top = q.top();
+            q.pop();
+            Node* v = top.second;
+            if (v->dst != top.first) {
+                continue;
+            } // handle duplicates
+            Node* u = v->next;
+            while (u!= nullptr) {
+                if (!udAdjList[u->index]->visited && udAdjList[u->index]->dst > u->weight) {
+                    udAdjList[u->index]->dst = u->weight;
+                    q.push(make_pair(udAdjList[u->index]->dst, udAdjList[u->index]));
+                    weightSum+=u->weight;
+                }
+                u = u->next;
+            }
+            top.second->visited = true;
+        }
+        bool allVisited = true;
+        for (uInt i=0; i<this->size; i++) {
+            allVisited &= this->udAdjList[i]->visited;
+        }
+        if (!allVisited) {
+            cout << "No MST exists!";
+            return;
+        }
+        cout << "The total weight of MST is " << weightSum << "\n";
+    }
 };
 
 
@@ -103,11 +193,15 @@ int main() {
     istringstream iStream;
     vector<Edge> e;
     uInt edgeStart, edgeDest, edgeWeight;
-    while (getline(cin, str)) {
-        iStream.str(str);
-        iStream >> edgeStart >> edgeDest >> edgeWeight;
+    while (!cin.eof()) {
+        cin >> edgeStart >> edgeDest >> edgeWeight;
         e.push_back(Edge(edgeStart, edgeDest, edgeWeight));
     }
     Graph g = Graph(size, start, destination, e);
-    g.findShortestPath();
+    g.findShortesPath();
+    g.isDAG();
+    g.findMST();
 }
+//getline(cin, str);
+//iStream.str(str);
+//iStream
